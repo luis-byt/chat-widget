1083a334f27038fdca13d2c5fe5171242d4d8999
luis-byt <luiso.byt@gmail.com>
2025-12-23 12:31:55 -0500

    Agregando el badge en color rojo para las notificaciones en el launcher

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,6 @@
 import terser from "@rollup/plugin-terser"
 
-const version = "1.0.4"
+const version = "1.0.5"
 const banner = `/*!
  * LuisByt Chat Widget v${version}
  * https://github.com/luis-byt/chat-widget
